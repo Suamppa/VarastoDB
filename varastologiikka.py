cc86a8b86f91453a34ff94b7f24e80a5afcf560b
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-10-20 22:37:37 +0300

    Add documentation to varastologiikka.py

--- varastologiikka.py
+++ varastologiikka.py
@@ -3,6 +3,20 @@ import random
 import sqlite3 as sql
 
 class Connection:
+    """
+    A context manager for connecting to a SQLite database.
+
+    Args:
+        db_path (str): The path to the SQLite database file.
+
+    Attributes:
+        db_path (str): The path to the SQLite database file.
+        _connection (sqlite3.Connection): The connection object to the database.
+
+    Methods:
+        __enter__(): Enters the context and returns the connection object.
+        __exit__(exc_type, exc_val, exc_tb): Exits the context and closes the connection.
+    """
     def __init__(self, db_path):
         self.db_path = db_path
         self._connection = None
@@ -18,11 +32,33 @@ class Connection:
 
 # Siirtoaika, Lavanumero, Sijainti
 def lavasiirto(cur: sql.Cursor, lava, minne):
+    """
+    Moves a pallet from one location to another and logs the transaction in the database.
+
+    Args:
+        cur (sqlite3.Cursor): The cursor object for the database connection.
+        lava (int): The pallet number to be moved.
+        minne (int): The id of the new location of the pallet.
+
+    Returns:
+        None
+    """
     cur.execute("UPDATE LAVA SET Sijainti = ? WHERE Lavanumero = ?", (minne, lava))
     cur.execute("INSERT INTO SIIRTOTAPAHTUMA VALUES (?, ?, ?)",
                 (datetime.datetime.now().isoformat(" ", "seconds"), lava, minne))
 
 def randdate(yrange=[2024, 2027], mrange=[1, 12], drange=[1, 31]):
+    """
+    Returns a random date as a string in ISO format (YYYY-MM-DD).
+
+    Args:
+        yrange (list): A list of two integers representing the range of years to choose from (inclusive).
+        mrange (list): A list of two integers representing the range of months to choose from (inclusive).
+        drange (list): A list of two integers representing the range of days to choose from (inclusive).
+
+    Returns:
+        str: A string representing a random date in ISO format (YYYY-MM-DD).
+    """
     y = random.randint(yrange[0], yrange[1])
     m = random.randint(mrange[0], mrange[1])
     if drange[1] > 28:
