779f10efcf709d77b07c41e91de6fb9997703de2
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-10-21 01:17:51 +0300

    Add a test suite to avoid database conflicts

---
+++ tests/test_group.py
@@ -0,0 +1,9 @@
+import unittest
+from test_create_db import TestCreateDB
+from test_varastologiikka import TestVarastoLogiikka
+
+class TestGroup(unittest.TestSuite):
+    def __init__(self):
+        super().__init__()
+        self.addTest(unittest.makeSuite(TestCreateDB))
+        self.addTest(unittest.makeSuite(TestVarastoLogiikka))
