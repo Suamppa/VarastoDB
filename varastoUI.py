796e603fdda0e4e077e41e2a6af761cfdc6b5274
Samppa Alatalo <samppa.alatalo@gmail.com>
2023-10-22 00:17:29 +0300

    Make use of the new views from commit db9961a to greatly clean up the scroll_info() function.

--- varastoUI.py
+++ varastoUI.py
@@ -7,9 +7,12 @@ def search_db(db: vl.Database):
         options = {str(i+1): label for i, label in enumerate(labels)}
         options["0"] = "Palaa"
         choice = vl.handle_input(options)
+        
         if choice == "0": # Palaa
             return
-        elif choice == "1": # Tuotteet
+        
+        search_term = input("Hakusana: ")
+        if choice == "1": # Tuotteet
             pass
         elif choice == "2": # Erät
             pass
@@ -22,6 +25,7 @@ def search_db(db: vl.Database):
         elif choice == "6": # Siirtotapahtumat
             pass
 
+# Selaa tietoja
 def scroll_info(db: vl.Database):
     while True: 
         print("Selaa:")
@@ -29,61 +33,23 @@ def scroll_info(db: vl.Database):
         options = {str(i+1): label for i, label in enumerate(labels)}
         options["0"] = "Palaa"
         choice = vl.handle_input(options)
+        
         if choice == "0": # Palaa
             return
         elif choice == "1": # Tuotteet
-            table = ("""
-                     TUOTE T LEFT OUTER JOIN ERÄ E ON T.Tuotenumero = E.Tuotenumero
-                     GROUP BY T.Tuotenumero
-                     """)
-            select = "T.*, COUNT(E.Eränumero) AS Erämäärä"
-            db.print_table(table, select)
+            db.print_table("TUOTETIEDOT")
         elif choice == "2": # Erät
-            table = ("""
-                     ERÄ E JOIN TUOTE T ON E.Tuotenumero = T.Tuotenumero
-                     LEFT OUTER JOIN ERÄ_LAVALLA EL ON E.Eränumero = EL.Eränumero
-                     LEFT OUTER JOIN LAVA L ON EL.Lavanumero = L.Lavanumero
-                     LEFT OUTER JOIN SIJAINTI S ON L.Sijainti = S.STunniste
-                     """)
-            select = ("""
-                      T.Nimi, T.Valmistaja, E.*, T.Tuoteryhmä, T.Säilytyslt, EL.Lavanumero, L.Tyyppi,
-                      SIJAINTI_STR(S.Hyllyväli, S.Sektio, S.Kerros, S.Kuormaruutu) AS Sijainti,
-                      S.Varasto
-                      """)
-            db.print_table(table, select)
+            db.print_table("TUOTEERÄT")
         elif choice == "3": # Lavapaikat
-            table = ("""
-                     SIJAINTI S LEFT OUTER JOIN LAVA L ON S.STunniste = L.Sijainti
-                     LEFT OUTER JOIN ERÄ_LAVALLA EL ON L.Lavanumero = EL.Lavanumero
-                     LEFT OUTER JOIN ERÄ E ON EL.Eränumero = E.Eränumero
-                     LEFT OUTER JOIN TUOTE T ON E.Tuotenumero = T.Tuotenumero
-                     """)
-            select = ("""
-                      SIJAINTI_STR(S.Hyllyväli, S.Sektio, S.Kerros, S.Kuormaruutu) AS Sijainti,
-                      S.Varasto, L.Lavanumero, L.Tyyppi, T.Nimi, E.PE_pvm, E.Ltk_määrä
-                      """)
-            db.print_table(table, select)
+            db.print_table("LAVAPAIKAT")
         elif choice == "4": # Lavat
-            table = ("""
-                     LAVA L LEFT OUTER JOIN SIJAINTI S ON L.Sijainti = S.STunniste
-                     LEFT OUTER JOIN ERÄ_LAVALLA EL ON L.Lavanumero = EL.Lavanumero
-                     LEFT OUTER JOIN ERÄ E ON EL.Eränumero = E.Eränumero
-                     LEFT OUTER JOIN TUOTE T ON E.Tuotenumero = T.Tuotenumero
-                     LEFT OUTER JOIN SIIRTOTAPAHTUMA ST ON L.Lavanumero = ST.Lavanumero
-                     GROUP BY L.Lavanumero
-                     """)
-            select = ("""
-                      L.Lavanumero, L.Tyyppi,
-                      SIJAINTI_STR(S.Hyllyväli, S.Sektio, S.Kerros, S.Kuormaruutu) AS Sijainti,
-                      E.Ltk_määrä, T.Nimi AS Tuotenimi, E.PE_pvm, ST.Siirtoaika
-                      """)
-            db.print_table(table, select)
+            db.print_table("LAVATIEDOT")
         elif choice == "5": # Varastot
-            db.print_table("VARASTO", "VTunniste AS Tunniste, Osoite")
+            db.print_table("VARASTOTIEDOT")
 
 def main():
-    print("Tervetuloa varastonhallintajärjestelmään!\n")
-    db = vl.Database(vl.Connection("varasto.db"))
+    print("\nTervetuloa varastonhallintajärjestelmään!\n")
+    db = vl.Database("varasto.db")
     while True:
         options = {"1": "Haku", "2": "Selaa tietoja", "3": "Lisää tietoja", "0": "Poistu"}
         choice = vl.handle_input(options)
